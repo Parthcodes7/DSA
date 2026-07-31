4bea7e0aec2f2b79c848d84de13d0736b592926d
Parthcodes7 <parthcodes7@users.noreply.github.com>
2026-07-31 23:05:04 +0530

    Upload 02_Sorting folder

---
+++ 02_Sorting/bubble_sort.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int arr[], int n) {
+    for (int i = n - 1; i >= 1; i--) {
+        int did_swap = 0;
+
+        for (int j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                did_swap = 1;
+            }
+        }
+
+        if (did_swap == 0) {
+            break;
+        }
+
+        cout << "runs\n";
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    bubble_sort(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
